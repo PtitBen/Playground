b0c34cd51dd941b9915691cc49c33bb69dc51d2b
Ben Franck <benjamin.franck@centrica.com>
2024-04-25 17:07:32 +0100

    Update docusaurus.config.js

--- cms-handbook/docusaurus.config.js
+++ cms-handbook/docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'CMS handbook',
+  tagline: 'Atoms are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -20,8 +20,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ptitben', // Usually your GitHub org/user name.
+  projectName: 'Playground', // Usually your repo name.
+  deploymentBranch: 'main',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
